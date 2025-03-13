de852bd4b0802555eab38da8bf034221fc60813c
Your Name <you@example.com>
2025-03-13 19:39:37 +0700

    allow host vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Cho phép truy cập từ bên ngoài
+    allowedHosts: ['thriving-charisma-production.up.railway.app'], // Thêm domain Railway vào danh sách cho phép
+  },
 })
