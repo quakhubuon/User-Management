ff430bcd6412a28ac3281fc77261fc2a8a2c13a4
Your Name <you@example.com>
2025-03-14 12:22:21 +0700

    fix user role

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Validator;
+use Spatie\Permission\Models\Role;
   
   
 class AuthController extends Controller
